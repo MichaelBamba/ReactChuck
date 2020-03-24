c5c53a3b170e4cd1005f31e75d02d1eb59312fcc
MichaelBamba <mjbamba2@gmail.com>
2020-03-24 13:38:00 -0400

    created chuck says ugly header and a simple quote component

---
+++ src/components/quote.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Quote extends Component {
+  render() {
+    return (
+      <div>
+        <p> Covid-19 is afraid of being caught by Chuck Norris </p>
+      </div>
+    );
+  }
+}
+export default Quote;
